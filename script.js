6c0af1ff4a31d44d0436a9f32760a314262de268
mcalex468 <alexmaco468@gmail.com>
2025-02-14 13:31:10 +0100

    commit mastermind

--- script.js
+++ script.js
@@ -15,27 +15,60 @@ var aciertos = 0;
 
 function init() {
     //1. Genera el código random del master
-
+    //masterColors()
     //2. Crea todas las filas según el número de intentos.
+    filasJuego()
 }
 
+/*Funcion colores del master*/
+function masterColors() {
+    let colorsMaster = document.getElementById('master');
+
+    for (let i = 0; i < MAX_COMBI_COLORES; i++) {
+        const i = Math.floor(Math.random() * COLORS.length);
+        master.push(COLORS[i]);
+    }
+    return master;
+    master.appendChild(colorsMaster);
+}
+
+console.log(masterColors());
+
+
+/** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
+function añadeColor(color) {
+    do {
+        let colorSelect = document.getElementById('combiText');  // Obtener el campo de texto
+        colorSelect.value += color + " ,";  // Añadir el color al valor del campo de texto
+        intento++
+    } while (intento < 5)
+    
+}
+
+
+function filasJuego() {
+    // Obtener el contenedor donde se agregarán las filas de intentos
+    let resultFilas = document.getElementById('Result');
+    for (let i = 0; i < MAX_INTENTOS; i++) {
+        // Crear una fila
+        resultFilas.innerHTML += ROW_RESULT;
+    }
+}
 
 
 /* Llamaremos a esta función desde el botón HTML de la página para comprobar la propuesta de combinación que nos ha
 introducido el usuario.
 Informamos al usuario del resultado y del número de intentos que lleva*/
 function Comprobar() {
+    // switch case
+    // usar variables globales
 }
 
-/** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
-function añadeColor(color) {
-   
-}
 
 
 /** Template con el código HTML que corresponde a cada fila de juego/intento. */
-const ROW_RESULT = `<div class="rowResult w100 flex wrap">
-    <div class="rowUserCombi w75 flex wrap">
+const ROW_RESULT = `<div class="rowResult w100 flex">
+    <div class="rowUserCombi w75 flex">
        <div class="w25">
            <div class="celUserCombi flex"></div>
        </div>
@@ -48,19 +81,19 @@ const ROW_RESULT = `<div class="rowResult w100 flex wrap">
        <div class="w25">
            <div class="celUserCombi flex"></div>
        </div>
-    </div>alis/Mastermind_CODIGO
-    <div class="rowCercleResult w25 flex wrap center">
-       <div class="w40 h40">
+    </div>
+    <div class="rowCercleResult w25 flex">
+       <div class="w50 h40">
             <div class="cercleResult flex"></div>
        </div>
-       <div class="w40 h40">
+       <div class="w50 h40">
            <div class="cercleResult flex"></div>
        </div>
-       <div class="w40 h40">
+       <div class="w50 h40">
            <div class="cercleResult flex"></div>
        </div>
-       <div class="w40 h40">
+       <div class="w50 h40">
            <div class="cercleResult flex"></div>
        </div>
-    <div>
+    </div>
 </div>`;
